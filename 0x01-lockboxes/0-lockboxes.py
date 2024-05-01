6f11dbd544b51b6b0646c69e05fa5e39231ee4e9
Ailwei <Ailweimaemu@gmail.com>
2024-05-01 05:24:13 -0700

    fixed the pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """A module for determining if all boxes can be opened from a list of lists."""
 
+
 def canUnlockAll(boxes=[]):
     """
     Determines if all the boxes can be opened.
 
     Args:
-    - boxes (list of list of int): A list of boxes, where each box is represented by a list of integers
-                                   indicating the keys it contains. Defaults to an empty list.
-
+    - boxes (list of list of int): A list of boxes,
+    where each box is represented by a list of integers
     Returns:
     - bool: True if all boxes can be opened, False otherwise.
     """
@@ -25,6 +25,7 @@ def canUnlockAll(boxes=[]):
         return False
 
     return True
+
 
 if __name__ == '__main__':
     boxes = [
