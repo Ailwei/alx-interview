98a16561df5aaf82d012093a876b42f3a4475274
Ailwei <Ailweimaemu@gmail.com>
2024-05-01 03:36:29 -0700

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
+"""A module for determining if all boxes can be opened from a list of lists."""
 
-from collections import deque
-"""
-A python module that stermines if all boxes can can be opened from a list of lists
-"""
-
-def canUnlockAll(boxes):
+def canUnlockAll(boxes=[]):
     """
     Determines if all the boxes can be opened.
 
     Args:
     - boxes (list of list of int): A list of boxes, where each box is represented by a list of integers
-                                   indicating the keys it contains.
+                                   indicating the keys it contains. Defaults to an empty list.
 
     Returns:
     - bool: True if all boxes can be opened, False otherwise.
@@ -19,19 +15,16 @@ def canUnlockAll(boxes):
     if not boxes:
         return False
 
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-    queue = deque([0])
+    keys = set([0])
+    for box_id, box in enumerate(boxes):
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
 
-    while queue:
-        box = queue.popleft()
-        for key in boxes[box]:
-            if 0 <= key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
+    if len(keys) != len(boxes):
+        return False
 
-    return all(visited)
+    return True
 
 if __name__ == '__main__':
     boxes = [
@@ -40,13 +33,13 @@ if __name__ == '__main__':
                 [3, 0],
                 [1, 2, 3],
             ]
-    print(canUnlockAll(boxes))
+    print(unlockBoxes(boxes))
 
     boxes = [[1], [2], [3], [4], []]
     print(canUnlockAll(boxes))
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
+    print(unlockBoxes(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
+    print(unlockBoxes(boxes))
