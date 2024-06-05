78787ad50c9c672d8b9aa5f5ec0b16d75683cd6b
Ailwei <Ailweimaemu@gmail.com>
2024-06-05 06:04:22 -0700

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,40 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
 if (!movieId) {
   console.error('Usage: ./0-star_wars_characters.js <movie_id>');
   process.exit(1);
 }
 
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
+request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
-  
+
   const filmData = JSON.parse(body);
-  const characterUrls = filmData.characters;
-  
-  characterUrls.forEach(url => {
-    request(url, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
+  const characters = filmData.characters;
+
+  if (characters && characters.length > 0) {
+    fetchCharacter(0, characters);
+  }
 });
+
+function fetchCharacter(index, characters) {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    fetchCharacter(index + 1, characters);
+  });
+}
