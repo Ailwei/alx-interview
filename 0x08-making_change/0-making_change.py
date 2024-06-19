b68b37eb976f6755e5ed40b57af13ea50ab69b30
Ailwei <Ailweimaemu@gmail.com>
2024-06-19 05:39:08 -0700

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Making change O(n)"""
 
+
 def makeChange(coins, total):
     """
      Determines the fewest number of coins needed to meet
@@ -8,15 +9,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+
+        return dp[total] if dp[total] != float('inf') else -1
+
 
 if __name__ == '__main__':
     print(makeChange([1, 2, 25], 37))
